6d03ab0f3f908f9666f7e9313c1757436556f54e
KiranT <kiran.thanduri@gmail.com>
2026-01-14 16:25:11 -0600

    5. Fifth Commit with Helper2 branch

--- first.py
+++ first.py
@@ -1,3 +1,4 @@
 print("Jist initiated the git and activated the main branch")
 print("Created a 2nd commit ")
 print("Created a 3rd commit ")
+print("Created a 5th commit with anoter helper branch ")
